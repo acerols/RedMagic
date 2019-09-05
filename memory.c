b933c74e0ce3b6cb03be776551ecda63a97843f4
acerols <arizexy@gmail.com>
2019-09-05 18:40:14 +0900

    modified:   kernel.S 	modified:   kernel.c 	modified:   memory.c 	modified:   multiboot.S 	modified:   os.iso
    	Makefile
    	include/terminal.h
    	include/vga.h
    	terminal.c

--- memory.c
+++ memory.c
@@ -4,6 +4,6 @@
 
 void initBSS(void)
 {
-    kmemset(&_bss_start, 0x00, ((unsigned int)&_bss_end - (unsigned int)&_bss_start));
+    kmemset((void *)&_bss_start, 0x00, ((unsigned int)&_bss_end - (unsigned int)&_bss_start));
 }
 
